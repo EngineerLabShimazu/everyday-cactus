f4d08d590fed2ae563e38deca731771a7449d5ed
GenkiTech <shimazu.g.r@gmail.com>
2019-12-28 22:58:18 +0900

    first change codestar template

--- index.py
+++ index.py
@@ -4,9 +4,11 @@ import datetime
 
 def handler(event, context):
     data = {
-        'output': 'Hello World',
+        'output': 'Hello World with first change',
         'timestamp': datetime.datetime.utcnow().isoformat()
-    }
-    return {'statusCode': 200,
-            'body': json.dumps(data),
-            'headers': {'Content-Type': 'application/json'}}
+        }
+    return {
+        'statusCode': 200,
+        'body': json.dumps(data),
+        'headers': {'Content-Type': 'application/json'}
+        }
